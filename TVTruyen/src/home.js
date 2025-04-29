5ecdd5969e86c5e93f9dbcc6b82b664263f27991
longvuu <herryvu.dev@gmail.com>
2025-04-29 23:04:29 +0700

    up

--- TVTruyen/src/home.js
+++ TVTruyen/src/home.js
@@ -1,9 +1,7 @@
 function execute() {
     return Response.success([
-        {
-            title: "Truyện Mới", input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-moi", script:"source.js",
-            //title: "Truyện Hot", input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-hot", script:"source.js",
-            //title: 'Truyên Full', input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-full", script:"source.js",
-        },
+            {title: "Truyện Mới", input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-moi", script:"source.js"},
+            {title: "Truyện Hot", input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-hot", script:"source.js"},
+            {title: 'Truyên Full', input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-full", script:"source.js"}
     ]);
 }
