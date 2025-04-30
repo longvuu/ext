5f44724637e1884423fac5e7ea113e6ab88defe5
longvuu <herryvu.dev@gmail.com>
2025-04-30 09:57:19 +0700

    up

--- TVTruyen/src/chap.js
+++ TVTruyen/src/chap.js
@@ -3,5 +3,8 @@ function execute(url) {
     // Chuyển đổi sang CDN
     let cdnUrl = url.replace(/^https?:\/\/(www\.)?tvtruyen\.com\//, "https://cdn.cscldsck.com/chapters/");
     let response = fetch(cdnUrl);
-    return Response.success(response.text());
+    if (response && response.ok) {
+        return Response.success(response.text());
+    }
+    return Response.error("Không lấy được nội dung chương từ CDN.");
 }
