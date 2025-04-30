a7c133ae5047462b0b8e6d84a70373599c6e2403
longvuu <herryvu.dev@gmail.com>
2025-04-30 10:15:31 +0700

    up

--- TVTruyen/src/chap.js
+++ TVTruyen/src/chap.js
@@ -2,7 +2,6 @@ load('config.js');
 function execute(url, reference) {
     // Chuyển đổi sang CDN
     let cdnUrl = url.replace(/^https?:\/\/(www\.)?tvtruyen\.com\//, "https://cdn.cscldsck.com/chapters/");
-    print("CDN URL: " + cdnUrl);
     let response = fetch(cdnUrl, { referer: reference });
     if (response && response.ok) {
        return Response.success(response.text());
