9dbc2391f60f162d4e11ec6f528db8241c13ba11
longvuu <herryvu.dev@gmail.com>
2025-05-03 22:17:58 +0700

    up

--- akaytruyen/src/toc.js
+++ akaytruyen/src/toc.js
@@ -15,7 +15,8 @@ function execute(url) {
         const seen = new Set();
         // Lấy nhanh tất cả chương từ selector tối ưu
         pageDoc.select('.story-detail__list-chapter--list ul.chapter-list a').forEach(e => {
-            let name = e.text().trim();
+            let name = e.select('.chapter-title.ms-2').text().trim();
+            if (!name) return;
             if (!seen.has(name)) {
                 seen.add(name);
                 allChapters.push({
