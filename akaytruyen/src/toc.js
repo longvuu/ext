81c85d90331031816613b6f79475aeca5c19219b
longvuu <herryvu.dev@gmail.com>
2025-05-03 21:36:27 +0700

    up

--- akaytruyen/src/toc.js
+++ akaytruyen/src/toc.js
@@ -2,20 +2,34 @@ load('config.js');
 
 function execute(url) {
     let allChapters = [];
-    let currentPage = 1;
-    let hasNext = true;
+    let maxPage = 1;
 
     // Loại bỏ ?page nếu có trong url gốc
     url = url.replace(/\?page=\\d+/, '');
 
-    while (hasNext) {
-        let pageUrl = url + "?page=" + currentPage;
-        let response = fetch(pageUrl);
-        if (!response.ok) break;
+    // Fetch trang đầu để lấy số trang lớn nhất
+    let response = fetch(url + "?page=1");
+    if (!response.ok) return Response.success(allChapters);
 
-        let doc = response.html();
-        // Lấy tất cả link chương
-        doc.select('.chapter-list a').forEach(e => {
+    let doc = response.html();
+
+    // Tìm số trang lớn nhất từ các <a> có data-url trong .pagination__item (bỏ disabled, lấy số lớn nhất)
+    doc.select('li.pagination__item a[data-url]').forEach(a => {
+        let pageUrl = a.attr('data-url');
+        let match = pageUrl.match(/page=(\\d+)/);
+        if (match) {
+            let pageNum = parseInt(match[1]);
+            if (pageNum > maxPage) maxPage = pageNum;
+        }
+    });
+
+    // Lặp qua từng trang để lấy danh sách chương
+    for (let currentPage = 1; currentPage <= maxPage; currentPage++) {
+        let pageUrl = url + "?page=" + currentPage;
+        let pageResponse = fetch(pageUrl);
+        if (!pageResponse.ok) break;
+        let pageDoc = pageResponse.html();
+        pageDoc.select('.chapter-list a').forEach(e => {
             let name = e.select('.chapter-title').text().trim();
             if (!name) name = e.text().trim();
             allChapters.push({
@@ -24,31 +38,6 @@ function execute(url) {
                 host: BASE_URL
             });
         });
-
-        // Phân trang: lấy tất cả mũi tên, chọn page lớn nhất
-        let nextPage = null;
-        let maxPage = currentPage;
-        let arrows = doc.select('li.pagination__arrow.pagination__item a[data-url]');
-        if (arrows && arrows.size() > 0) {
-            arrows.forEach(a => {
-                let nextUrl = a.attr('data-url');
-                let match = nextUrl.match(/page=(\\d+)/);
-                if (match) {
-                    let pageNum = parseInt(match[1]);
-                    if (pageNum > maxPage) {
-                        maxPage = pageNum;
-                    }
-                }
-            });
-            if (maxPage > currentPage) {
-                nextPage = maxPage;
-            }
-        }
-        if (nextPage) {
-            currentPage = nextPage;
-        } else {
-            hasNext = false;
-        }
     }
 
     return Response.success(allChapters);
