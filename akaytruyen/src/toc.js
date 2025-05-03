2d513303d89ebff94bd31f825f4bf233fcc23022
longvuu <herryvu.dev@gmail.com>
2025-05-03 22:04:43 +0700

    up

--- akaytruyen/src/toc.js
+++ akaytruyen/src/toc.js
@@ -3,27 +3,10 @@ load('config.js');
 function execute(url) {
     let allChapters = [];
     let maxPage = 1;
-
-    // Loại bỏ ?page nếu có trong url gốc
-    url = url.replace(/\?page=\\d+/, '');
-
-    // Fetch trang đầu để lấy số trang lớn nhất
-    let response = fetch(url + "?page=1");
-    if (!response.ok) return Response.success(allChapters);
-
-    let doc = response.html();
-
-    // Tìm số trang lớn nhất từ các <a> có data-url trong .pagination__item (bỏ disabled, lấy số lớn nhất)
-    doc.select('li.pagination__item a[data-url]').forEach(a => {
-        let pageUrl = a.attr('data-url');
-        let match = pageUrl.match(/page=(\\d+)/);
-        if (match) {
-            let pageNum = parseInt(match[1]);
-            if (pageNum > maxPage) maxPage = pageNum;
-        }
-    });
-
-    // Lặp qua từng trang để lấy danh sách chương
+    url = url.replace(/\?page=\d+/, '');
+    if (url.indexOf("/truyen/con-duong-ba-chu") !== -1) {
+        maxPage = 100;
+    }
     for (let currentPage = 1; currentPage <= maxPage; currentPage++) {
         let pageUrl = url + "?page=" + currentPage;
         let pageResponse = fetch(pageUrl);
