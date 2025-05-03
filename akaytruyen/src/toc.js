7e4770fd14a657ed5e6c6f4c3d6439d1e420f99d
longvuu <herryvu.dev@gmail.com>
2025-05-03 20:43:57 +0700

    up

--- akaytruyen/src/toc.js
+++ akaytruyen/src/toc.js
@@ -5,12 +5,28 @@ function execute(url, page) {
     let currentPage = page ? parseInt(page) : 1;
     let hasNext = true;
 
-    // Đảm bảo url luôn có dạng https://akaytruyen.com/truyen/xxx?page=1
-    // Loại bỏ tham số ?page nếu có sẵn trong url
+    // Đảm bảo url luôn có dạng https://akaytruyen.com/truyen/xxx (không có ?page)
     url = url.replace(/\?page=\d+/, '');
 
     while (hasNext) {
+        // Luôn fetch với url gốc + ?page=currentPage
         let pageUrl = url + "?page=" + currentPage;
+
+        // Nếu không phải trang đầu tiên, kiểm tra pagination__arrow pagination__item để lấy link mới
+        if (currentPage !== 1) {
+            let tempResponse = fetch(url + "?page=" + currentPage);
+            if (!tempResponse.ok) break;
+            let tempDoc = tempResponse.html();
+            // Lấy đúng mũi tên phân trang (>>)
+            let arrowLi = tempDoc.select('li.pagination__arrow.pagination__item a[data-url]');
+            if (arrowLi && arrowLi.size() > 0) {
+                let arrowUrl = arrowLi.attr('data-url');
+                if (arrowUrl) {
+                    pageUrl = arrowUrl;
+                }
+            }
+        }
+
         let response = fetch(pageUrl);
         if (!response.ok) break;
 
@@ -26,9 +42,9 @@ function execute(url, page) {
             });
         });
 
-        // Kiểm tra trang tiếp theo
+        // Kiểm tra trang tiếp theo (chỉ lấy mũi tên sang phải >>)
         let nextPage = null;
-        let nextArrow = doc.select('.pagination__arrow a[data-url]');
+        let nextArrow = doc.select('li.pagination__arrow.pagination__item a[data-url]');
         if (nextArrow && nextArrow.size() > 0) {
             let nextUrl = nextArrow.attr('data-url');
             let match = nextUrl.match(/page=(\d+)/);
