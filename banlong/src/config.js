880080278be88e7bc019f707af6e294ac32d6fbf
longvuu <herryvu.dev@gmail.com>
2025-06-28 17:43:28 +0700

    update banlong

---
+++ banlong/src/config.js
@@ -0,0 +1,7 @@
+let BASE_URL = "https://banlong.vip";
+try {
+    if (CONFIG_URL) {
+        BASE_URL = CONFIG_URL;
+    }
+} catch (error) {
+}
---
+++ banlong/src/toc.js
@@ -0,0 +1,20 @@
+load("config.js");
+
+function execute(url) {
+    let response = fetch(BASE_URL.replace("https://", "https://api.") + "/" + url);
+    if (response.ok) {
+        let json = response.json();
+        let chapters = [];
+        json.data.forEach(e => {
+            chapters.push({
+                name: e.name,
+                url: e.url,
+                pay: e.is_vip,
+                host: BASE_URL,
+            });
+        });
+        return Response.success(chapters);
+    }
+    return null;
+
+}
